14818dcd38dbf70624ae1a21f3432ca4b740a59b
PetkoKuzmanov <57803685+PetkoKuzmanov@users.noreply.github.com>
2020-12-27 22:36:27 +0200

    Added quick add

--- app/src/main/java/com/example/project/addFood/AddFoodActivity.kt
+++ app/src/main/java/com/example/project/addFood/AddFoodActivity.kt
@@ -1,14 +1,52 @@
 package com.example.project.addFood
 
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.EditText
 import com.example.project.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.util.*
 
 class AddFoodActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_food)
         setSupportActionBar(findViewById(R.id.addFoodToolBar))
+
+    }
+
+    fun quickAdd(view: View) {
+        val inflater = LayoutInflater.from(this)
+        val quickAddView = inflater.inflate(R.layout.quick_add_layout, null)
+        val enterCalories = quickAddView.findViewById(R.id.enterCalories) as EditText
+
+
+        //Create the alertDialog
+        val builder = AlertDialog.Builder(this)
+            .setView(quickAddView)
+            .setTitle("Quick add")
+        builder.create()
+        builder.setPositiveButton("ADD") { _, _ ->
+            val input = enterCalories.text.toString()
+
+
+            //Check if the topic can be added
+            if (!TextUtils.isEmpty(input)) {
+//                mAdapter.addItem(weight, dateTextView.text.toString())
+
+            }
+        }
+        builder.setNegativeButton("CANCEL") { _, _ ->
+            //Cancels the adding of the weight even if this is left empty
+        }
+        builder.show()
+    }
+
+    fun scanItem(view: View) {
 
     }
 }
