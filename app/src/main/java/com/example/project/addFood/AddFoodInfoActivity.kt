8897123d9b496a1a00156d0f21d05a8632cbd06d
PetkoKuzmanov <57803685+PetkoKuzmanov@users.noreply.github.com>
2020-12-28 20:49:54 +0200

    Tweaked addFoodInfo

--- app/src/main/java/com/example/project/addFood/AddFoodInfoActivity.kt
+++ app/src/main/java/com/example/project/addFood/AddFoodInfoActivity.kt
@@ -2,12 +2,11 @@ package com.example.project.addFood
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
-import androidx.core.widget.doAfterTextChanged
 import com.example.project.R
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
@@ -25,24 +24,6 @@ class AddFoodInfoActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.addFoodInfoToolbar))
 
         database = Firebase.database.reference
-
-        val nameEditText = findViewById<EditText>(R.id.nameEditText)
-        val descriptionEditText = findViewById<EditText>(R.id.descriptionEditText)
-        val measurementEditText = findViewById<EditText>(R.id.measurementEditText)
-        val carbohydratesEditText = findViewById<EditText>(R.id.carbohydratesEditText)
-        val fatsEditText = findViewById<EditText>(R.id.fatsEditText)
-        val proteinsEditText = findViewById<EditText>(R.id.proteinsEditText)
-        val caloriesTextView = findViewById<TextView>(R.id.addFoodCaloriesTextView)
-
-        carbohydratesEditText.doAfterTextChanged {
-            updateCalories(carbohydratesEditText.text, fatsEditText.text, proteinsEditText.text)
-        }
-        fatsEditText.doAfterTextChanged {
-            updateCalories(carbohydratesEditText.text, fatsEditText.text, proteinsEditText.text)
-        }
-        proteinsEditText.doAfterTextChanged {
-            updateCalories(carbohydratesEditText.text, fatsEditText.text, proteinsEditText.text)
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -50,91 +31,27 @@ class AddFoodInfoActivity : AppCompatActivity() {
         return super.onCreateOptionsMenu(menu)
     }
 
-    /*
-    Update the textView for the calories
-     */
-    private fun updateCalories(
-        carbohydrates: CharSequence,
-        fats: CharSequence,
-        proteins: CharSequence
-    ) {
-        val caloriesTextView = findViewById<TextView>(R.id.addFoodCaloriesTextView)
-
-        var carbohydratesCalories = 0
-        var fatsCalories = 0
-        var proteinsCalories = 0
-
-        if (carbohydrates.isNotEmpty()) {
-            carbohydratesCalories = carbohydrates.toString().toInt() * 4
-        }
-        if (fats.isNotEmpty()) {
-            fatsCalories = fats.toString().toInt() * 9
-        }
-        if (proteins.isNotEmpty()) {
-            proteinsCalories = proteins.toString().toInt() * 4
-        }
-
-        val caloriesInt = carbohydratesCalories + fatsCalories + proteinsCalories
-        caloriesTextView.text = caloriesInt.toString()
-    }
-
     fun addFoodInfo(item: MenuItem) {
-        val id = intent.getStringExtra("barcode")
         val name = findViewById<EditText>(R.id.nameEditText).text.toString()
         val description = findViewById<EditText>(R.id.descriptionEditText).text.toString()
         val measurement = findViewById<EditText>(R.id.measurementEditText).text.toString()
-        val carbohydrates = findViewById<EditText>(R.id.carbohydratesEditText).text
-        val fats = findViewById<EditText>(R.id.fatsEditText).text
-        val proteins = findViewById<EditText>(R.id.proteinsEditText).text
-        val calories = findViewById<TextView>(R.id.addFoodCaloriesTextView).text.toString().toInt()
-
-        //If a field is empty set it to zero
-        var carbohydratesInt = 0
-        var fatsInt = 0
-        var proteinsInt = 0
-
-        if (carbohydrates.isNotEmpty()) {
-            carbohydratesInt = carbohydrates.toString().toInt()
-        }
-        if (fats.isNotEmpty()) {
-            fatsInt = fats.toString().toInt()
-        }
-        if (proteins.isNotEmpty()) {
-            proteinsInt = proteins.toString().toInt()
-        }
+        val carbohydratesText = findViewById<EditText>(R.id.carbohydratesEditText).text
+        val fatsText = findViewById<EditText>(R.id.fatsEditText).text
+        val proteinsText = findViewById<EditText>(R.id.proteinsEditText).text
+        val caloriesText = findViewById<EditText>(R.id.addFoodCaloriesEditText).text
 
         //Add the food to the database if the name, description and measurement are not empty
         if (name.isNotEmpty()
             && description.isNotEmpty()
             && measurement.isNotEmpty()
         ) {
-            mAuth.currentUser?.let {
-                database.child("food").child(id!!).child("name").setValue(name)
-            }
-
-            mAuth.currentUser?.let {
-                database.child("food").child(id!!).child("description").setValue(description)
-            }
-
-            mAuth.currentUser?.let {
-                database.child("food").child(id!!).child("measurement").setValue(measurement)
-            }
-
-            mAuth.currentUser?.let {
-                database.child("food").child(id!!).child("carbohydrates").setValue(carbohydratesInt)
-            }
-
-            mAuth.currentUser?.let {
-                database.child("food").child(id!!).child("fats").setValue(fatsInt)
-            }
-
-            mAuth.currentUser?.let {
-                database.child("food").child(id!!).child("proteins").setValue(proteinsInt)
-            }
-
-            mAuth.currentUser?.let {
-                database.child("food").child(id!!).child("calories").setValue(calories)
-            }
+            addSingleFoodInfo("name", name)
+            addSingleFoodInfo("description", description)
+            addSingleFoodInfo("measurement", measurement)
+            addSingleFoodInfo("carbohydrates", carbohydratesText)
+            addSingleFoodInfo("fats", fatsText)
+            addSingleFoodInfo("proteins", proteinsText)
+            addSingleFoodInfo("calories", caloriesText)
             this.onBackPressed()
         } else {
             val toast = Toast.makeText(
@@ -143,6 +60,26 @@ class AddFoodInfoActivity : AppCompatActivity() {
                 Toast.LENGTH_LONG
             )
             toast.show()
+        }
+    }
+
+    private fun addSingleFoodInfo(infoName: String, info: String) {
+        val id = intent.getStringExtra("barcode")
+        mAuth.currentUser?.let {
+            database.child("food").child(id!!).child(infoName).setValue(info)
+        }
+    }
+
+    private fun addSingleFoodInfo(infoName: String, editable: Editable) {
+        val id = intent.getStringExtra("barcode")
+
+        var info = 0
+        if (editable.isNotEmpty()) {
+            info = editable.toString().toInt()
+        }
+
+        mAuth.currentUser?.let {
+            database.child("food").child(id!!).child(infoName).setValue(info)
         }
     }
 }
